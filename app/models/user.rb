99c993ef4c51d08d8519262ecbbb568a65b2e4f6
daniel.houle <daniel.houle@brehm.org>
2012-11-16 15:29:35 -0600

    Starting on singin and out

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,11 +17,18 @@ class User < ActiveRecord::Base
 	is_gravtastic
 
 	before_save { self.email.downcase! }
+	before_save :create_remember_token
 
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
 	validates :password, length: { minimum: 6 }
 	validates :password_confirmation, presence: true
+
+	private
+
+	def create_remember_token
+		self.remember_token = SecureRandom.urlsafe_bas64
+	end	
 end
 
